7fe77f38b0bac76de4ffa26a8150859bd65ed960
Ry Currier <rygc1999@gmail.com>
2026-02-02 11:38:33 -0500

    link `FilterRoot` fields to tree state using `juce::CachedValue`

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -23,13 +23,47 @@ using c128 = juce::dsp::Complex<double>;
 
 //#include "filterstate.h"
 
+namespace IDs
+{
+  const juce::Identifier FilterState("FILTER_STATE");
+  const juce::Identifier Root("Root");
+  const juce::Identifier Zeros("Zeros");
+  const juce::Identifier Poles("Poles");
+  const juce::Identifier Conjugate("Conjugate");
+  const juce::Identifier ValueRe("ValueReal");
+  const juce::Identifier ValueIm("ValueImag");
+  const juce::Identifier Order("Order");
+}
+
 struct FilterRoot
 {
   using Ptr = juce::WeakReference<FilterRoot>;
 
-  Ptr conjugate;
-  c128 value;
-  s32 order;
+  FilterRoot(juce::ValueTree v) : node(v)
+  {
+    conjugate.referTo(node, IDs::Conjugate, nullptr);
+    value.re.referTo(node, IDs::ValueRe, nullptr);
+    value.im.referTo(node, IDs::ValueIm, nullptr);
+    order.referTo(node, IDs::Order, nullptr);
+  }
+
+  juce::ValueTree node; // each root manages its own node in the state tree
+
+  juce::CachedValue<int> conjugate; // the index of this root's conjugate node in the state tree
+  struct {
+    juce::CachedValue<double> re;
+    juce::CachedValue<double> im;
+    void setValue(c128 v, juce::UndoManager *um)
+    {
+      re.setValue(v.real(), um);
+      im.setValue(v.imag(), um);
+    }
+  } value;
+  juce::CachedValue<int> order;
+
+  // Ptr conjugate;
+  // c128 value;
+  // s32 order;
 
 private:
   JUCE_DECLARE_WEAK_REFERENCEABLE(FilterRoot);
@@ -39,40 +73,64 @@ struct FilterState
 {
   FilterState(juce::AudioProcessor &p, juce::UndoManager *um)
     : apvts(p, um)
-  {}
+  {
+    if(!apvts.state.isValid())
+    {
+      apvts.state = juce::ValueTree(IDs::FilterState);
+    }
+    auto zerosNode = apvts.state.getOrCreateChildWithName(IDs::Zeros, um);
+    auto polesNode = apvts.state.getOrCreateChildWithName(IDs::Poles, um);
+  }
 
   FilterRoot::Ptr add(s32 newOrder)
   {
+    juce::ValueTree newNode(IDs::Root);
+    newNode.setProperty(IDs::Order, newOrder, nullptr);
+    newNode.setProperty(IDs::ValueRe, 0.0, nullptr);
+    newNode.setProperty(IDs::ValueIm, 0.0, nullptr);
+    newNode.setProperty(IDs::Conjugate, -1, nullptr);
+
     FilterRoot *root = nullptr;
     if(newOrder > 0)
     {
-      root = zeros.add(new FilterRoot());
-      root->order = newOrder;
+      // NOTE: adding a zero
+      apvts.state.getChildWithName(IDs::Zeros).appendChild(newNode, apvts.undoManager);
+      root = zeros.add(new FilterRoot(newNode));
     }
     else if(newOrder < 0)
     {
-      root = poles.add(new FilterRoot());
-      root->order = newOrder;
+      // NOTE: adding a pole
+      apvts.state.getChildWithName(IDs::Poles).appendChild(newNode, apvts.undoManager);
+      root = poles.add(new FilterRoot(newNode));
     }
     else
     {
       jassertfalse; // order must be nonzero;
     }
-    FilterRoot::Ptr result(root);
     order += u32(std::abs(newOrder));
+    FilterRoot::Ptr result(root);
     return(result);
   }
 
-  void remove(FilterRoot::Ptr root)
+  void remove(FilterRoot::Ptr rootRef)
   {
-    // NOTE: `removeObject` does nothing if the passed object is not in the array
-    zeros.removeObject(root.get());
-    poles.removeObject(root.get());
+    // NOTE: if the root has already been deleted, we don't need to do anything
+    if(auto root = rootRef.get())
+    {
+      auto node = root->node;
+      auto parent = node.getParent();
+      parent.removeChild(node, apvts.undoManager);
+
+      // NOTE: `removeObject` does nothing if the passed object is not in the array
+      zeros.removeObject(root);
+      poles.removeObject(root);
+    }
   }
 
   juce::OwnedArray<FilterRoot> zeros;
   juce::OwnedArray<FilterRoot> poles;
   u32 order;
+
 private:
   juce::AudioProcessorValueTreeState apvts;
 };
