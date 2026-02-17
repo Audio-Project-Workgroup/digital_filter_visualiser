9b829812d6b58cff797672cf1b8c8911b3c42b7b
justonem0reuser <anton.swdp@gmail.com>
2026-02-17 23:43:24 +0100

    Added RootsToCoefficients; updated gitignore.

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -3,24 +3,6 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
 
-using u8  = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
-
-using s8  = int8_t;
-using s16 = int16_t;
-using s32 = int32_t;
-using s64 = int64_t;
-
-using b32 = s32;
-
-using r32 = float;
-using r64 = double;
-
-using c64  = juce::dsp::Complex<float>;
-using c128 = juce::dsp::Complex<double>;
-
 #include "FilterState.h"
 
 //==============================================================================
---
+++ src/RootsToCoefficients.h
@@ -0,0 +1,88 @@
+#pragma once
+#include "FilterState.h"
+
+class RootsToCoefficients
+{
+public:
+	static std::vector<double> CalculatePolynomialCoefficientsFrom(juce::OwnedArray<FilterRoot>& roots)
+	{
+		// Determine polinomial order
+
+		int order = 0;
+		for (const auto* root : roots)
+		{
+			int rootOrder = root->order.get();
+			bool isReal = root->value.im.get() == 0;
+			order += isReal ? rootOrder : 2 * rootOrder;
+		}
+
+		// Sort roots by magnitude for reducing numerical mistakes
+
+		struct RootIndexes
+		{
+			int index;
+			double magnitude;
+		};
+
+		std::vector<RootIndexes> rootIndexes(roots.size());
+		for (int i = 0; i < roots.size(); i++)
+		{
+			rootIndexes[i].index = i;
+			rootIndexes[i].magnitude = std::abs(roots[i]->value.get());
+		}
+
+		std::sort(rootIndexes.begin(), rootIndexes.end(),
+			[](const RootIndexes& a, const RootIndexes& b)
+			{
+				return a.magnitude < b.magnitude;
+			});
+
+		// Calculate coefficients
+
+		std::vector<double> res(order + 1);
+		res[0] = 1;
+		int nonZeroCoeffCount = 1;
+
+		for (int i = 0; i < roots.size(); i++)
+		{
+			auto* root = roots[rootIndexes[i].index];
+			const double re = root->value.re.get();
+			const double im = root->value.im.get();
+			const int order = root->order.get();
+			if (im == 0) // One real root
+			{
+				const double a0 = -re;
+				const double a1 = 1.0;
+				for (int i = 0; i < order; i++)
+				{
+					for (int j = nonZeroCoeffCount - 1; j >= 0; j--)
+					{
+						double v = res[j];
+						res[j + 1] += v * a1;
+						res[j] = v * a0;
+					}
+					nonZeroCoeffCount += 1;
+				}
+			}
+			else //(im != 0) => Two conjugate roots
+			{
+				const double a0 = re * re + im * im;
+				const double a1 = -2.0 * re;
+				const double a2 = 1.0;
+				for (int i = 0; i < order; i++)
+				{
+					for (int j = nonZeroCoeffCount - 1; j >= 0; j--)
+					{
+						double v = res[j];
+						res[j + 2] += v * a2;
+						res[j + 1] += v * a1;
+						res[j] = v * a0;
+					}
+					nonZeroCoeffCount += 2;
+				}
+			}
+		}
+
+		return res;
+	}
+};
