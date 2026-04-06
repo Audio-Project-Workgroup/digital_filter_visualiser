ab3a0a987ac4bfa433bd424c7555ec1c9aeec3ee
Paschalis Melissas <melissaspaschalis@gmail.com>
2026-04-06 23:55:10 +0300

    make --parallel an optional flag to avoid crashes when trying to build

--- build.sh
+++ build.sh
@@ -3,11 +3,19 @@
 ROOT_DIR=$PWD
 SRC_DIR=$ROOT_DIR/src
 
+# flags
+PARALLEL_BUILD=""
+for arg in "$@"; do
+    if [ "$arg" = "--parallel" ]; then
+        PARALLEL_BUILD="--parallel"
+    fi
+done
+
 mkdir -p build
 pushd build > /dev/null
 
 BUILD_DIR=$PWD
 cmake -S $SRC_DIR -B $BUILD_DIR
-cmake --build $BUILD_DIR --parallel
+cmake --build $BUILD_DIR $PARALLEL_BUILD
 
 popd > /dev/null
