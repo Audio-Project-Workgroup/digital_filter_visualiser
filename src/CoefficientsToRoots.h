c90cb04fd8988ac3bcc210d4fb7dd99f985c64d2
Paschalis Melissas <melissaspaschalis@gmail.com>
2026-05-26 19:04:14 +0300

    implement updating of the filter state on coefficient edits

--- src/CoefficientsToRoots.h
+++ src/CoefficientsToRoots.h
@@ -2,17 +2,21 @@
 
 #include "FilterState.h"
 #include <vector>
+#include <utility>
+
+#define DEBUG_C2R
 
 class CoefficientsToRoots
 {
 	public:
-		static std::vector<c128> GramSchmidt(std::vector<double> coefs);
+		static std::vector<std::pair<c128, int>> GramSchmidt(std::vector<double> coefs); // TODO change return type to bool --> for when there is a pole outside the unit. Check will be done outside of this.
 	private:
 
 		// TODO Finetune these parameters
 	    static constexpr double Epsilon = 1e-3;
 	    static constexpr int MaxIterations = 100000;
+		static constexpr double tolerance = 1e-4; // threshold for considering two roots with negligible diff the same. Expressed in % after scaling differences, since zeros can much more than 1.0.
 
 		static void printCheck(const std::vector<std::vector<double>> &matrix);
-		static std::vector<c128> extractRoots(const std::vector<std::vector<double>>& M, int n);
+		static std::vector<std::pair<c128, int>> extractRoots(const std::vector<std::vector<double>>& M, int n);
 };
