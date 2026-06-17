850d965738e7199ddfcad7436b0c54a848c5e2d5
Liz Aryslanova <connect.with.liza@gmail.com>
2026-06-17 17:31:16 +0400

    Working Aberth, needs cleanup

--- src/CoeffToRootsAberth.h
+++ src/CoeffToRootsAberth.h
@@ -4,7 +4,6 @@
 //
 //  Created by Elizabeth Aryslanova on 02/06/2026.
 //
-#pragma once
 #include <vector>
 #include <complex>
 
@@ -21,13 +20,14 @@ private:
     using c_vector = std::vector<std::complex<float>>;
 
     static vector polynomial_coefficients(const vector& coefficients);
-    static vector derivative_coefficients(const vector& polynomial);
-    static c_vector initial_guesses(const vector& polynomial, int n);
+    static vector derivative_coefficients(const vector& polynomial, const int n);
+    static c_vector initial_guesses(const vector& polynomial);
     static c_vector newton_coefficients(const vector& polynomial, const vector& derivative, const c_vector& guesses);
     static c_vector thing_in_parenths(const c_vector& guesses);
-    static c_vector update_guesses(const c_vector& guesses);
+    static c_vector update_guesses(const vector& polynomial, const vector& derivative, const c_vector& guesses, c_vector& new_guesses);
     static bool continue_condition(const c_vector& prev_guesses, const c_vector& new_guesses);
 
     Aberth() = delete; // This class can't be instantiated, it's just a holder for static methods..
 };
+
 
