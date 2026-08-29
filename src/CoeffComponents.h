838cb1838038271ceaf60fd965ed030fea2f3d34
paschalis melissas <melissaspaschalis@gmail.com>
2026-08-29 18:27:43 +0200

    clean CoeffComponents header from unsused defines and comments

--- src/CoeffComponents.h
+++ src/CoeffComponents.h
@@ -7,7 +7,6 @@
 #include <string>
 
 #define TABLE_TITLE "Coefficients"
-#define DEFAULT_IS_EXPANDED 1
 
 class CoefficientsComponent final
     : public juce::Component
@@ -31,9 +30,6 @@ class CoefficientsComponent final
         void toggleCollapseExpand();
         void updateCoeffTable();
         void updateFilterStateOnCoefEdit(int row, int col, double value);
-
-        // override juce::Component
-        // void paint(juce::Graphics &g) override; // TODO is this trully needed?
 
         // override juce::TableListBox
         int getNumRows() override;
