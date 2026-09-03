9e6fa53dff7e0f94ffd5b1353de5e164de6418b0
Ry Currier <rygc1999@gmail.com>
2026-09-03 12:06:30 -0400

    C2R: common polynomial and root refinement helpers

--- src/CoefficientsToRoots.cpp
+++ src/CoefficientsToRoots.cpp
@@ -10,6 +10,131 @@
 namespace CoefficientsToRoots
 {
 
+  static c128
+  evaluatePolynomial(const Coefficients &coeffs, c128 pt)
+  {
+    c128 result = coeffs[0];
+    for(size_t i = 1; i < coeffs.size(); ++i)
+    {
+      auto coeff = coeffs[i];
+      result = pt*result + coeff;
+    }
+
+    return result;
+  }
+
+  static c128
+  evaluateComplexPolynomial(const ComplexCoefficients &coeffs, c128 pt)
+  {
+    c128 result = coeffs[0];
+    for(size_t i = 1; i < coeffs.size(); ++i)
+    {
+      auto coeff = coeffs[i];
+      result = pt*result + coeff;
+    }
+
+    return result;
+  }
+
+  static c128
+  dividePolynomial(const Coefficients &coeffs, c128 pt, ComplexCoefficients &quotient)
+  {
+    quotient.resize(coeffs.size());
+
+    c128 result = coeffs[0];
+    quotient[0] = result;
+
+    for(size_t i = 1; i < coeffs.size(); ++i)
+    {
+      auto coeff = coeffs[i];
+      result = pt*result + coeff;
+
+      if(i < coeffs.size() - 1)
+      { quotient[i] = result; }
+    }
+
+    return result;
+  }
+
+  static c128
+  divideComplexPolynomial(const ComplexCoefficients &coeffs, c128 pt, ComplexCoefficients &quotient)
+  {
+    quotient.resize(coeffs.size());
+
+    c128 result = coeffs[0];
+    quotient[0] = result;
+
+    for(size_t i = 1; i < coeffs.size(); ++i)
+    {
+      auto coeff = coeffs[i];
+      result = pt*result + coeff;
+
+      if(i < coeffs.size() - 1)
+      { quotient[i] = result; }
+    }
+
+    return result;
+  }
+
+  static void
+  dividePolynomialByRoot(const Coefficients &coeffs, Root pt, ComplexCoefficients &remainders)
+  {
+    jassert(pt.order <= int(coeffs.size()));
+    jassert(pt.order == int(remainders.size()));
+
+    auto remSize = static_cast<int>(remainders.size());
+
+    ComplexCoefficients quotientBacking(coeffs.size() - 1);
+    ComplexCoefficients *quotient = &quotientBacking;
+    remainders[static_cast<size_t>(remSize - 1)] = dividePolynomial(coeffs, pt.value, *quotient);
+    if(pt.order == 1) return;
+
+    ComplexCoefficients ccoeffsBacking(coeffs.size() - 1);
+    ComplexCoefficients *ccoeffs= &quotientBacking;
+    quotient = &ccoeffsBacking;
+    quotient->resize(quotient->size() - 1);
+
+    for(int n = 1; n < pt.order; ++n)
+    {
+      remainders[static_cast<size_t>(remSize - 1 - n)] = divideComplexPolynomial(*ccoeffs, pt.value, *quotient);
+      std::swap(ccoeffs, quotient);
+      quotient->resize(quotient->size() - 1);
+    }
+  }
+
+  static Root
+  mergeRoot(Root oldRoot, Root addedRoot)
+  {
+    Root newRoot;
+    if(juce::exactlyEqual(oldRoot.value.imag(), 0.0) == juce::exactlyEqual(addedRoot.value.imag(), 0.0))
+    {
+      newRoot.value = (oldRoot.value*c128(oldRoot.order, 0) + addedRoot.value) / c128(oldRoot.order + 1, 0);
+      newRoot.order = oldRoot.order + 1;
+    }
+    else
+    {
+      c128 reVal = (!juce::exactlyEqual(oldRoot.value.imag(), 0.0)) ? addedRoot.value : oldRoot.value;
+      c128 imVal = (!juce::exactlyEqual(oldRoot.value.imag(), 0.0)) ? c128(oldRoot.value.real(), 0) : c128(addedRoot.value.real(), 0);
+      int reOrder = (!juce::exactlyEqual(oldRoot.value.imag(), 0.0)) ? addedRoot.order : oldRoot.order;
+      int imOrder = (!juce::exactlyEqual(oldRoot.value.imag(), 0.0)) ? oldRoot.order : addedRoot.order;
+
+      newRoot.value = (reVal*c128(reOrder, 0) + c128(2, 0)*imVal*c128(imOrder, 0)) / c128(reOrder + 2*imOrder, 0);
+      newRoot.order = reOrder + 2*imOrder;
+    }
+
+    return newRoot;
+  }
+
+  static c128
+  centerOfCircleThroughPoints(c128 z0, c128 z1, c128 z2)
+  {
+    // TODO: check for colinearity, duplicates
+
+    c128 w = (z2 - z0) / (z1 - z0);
+    c128 c = (z1 - z0)*((w - std::norm(w))/(w - std::conj(w))) + z0;
+    return c;
+  }
+
 #include "CoefficientsToRoots.QR.cpp"
 #include "CoefficientsToRoots.Aberth.cpp"
 
--- src/CoefficientsToRoots.h
+++ src/CoefficientsToRoots.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "FilterState.h"
+#include <float.h>
 #include <vector>
 #include <utility>
 
@@ -8,6 +9,7 @@ namespace CoefficientsToRoots
 {
   struct Root
   {
+    Root(void) : value(0), order(0) {}
     Root(c128 v, int o) : value(v), order(o) {}
 
     c128 value;
@@ -15,8 +17,52 @@ namespace CoefficientsToRoots
   };
 
   using Coefficients = std::vector<double>;
+  using ComplexCoefficients = std::vector<c128>;
   using SolutionSet = std::vector<Root>;
   using SolverFn = SolutionSet (*)(Coefficients);
+
+  /** evaluates polynomial with real coefficients at complex point.
+      returns result
+  */
+  [[maybe_unused]] static c128 evaluatePolynomial(const Coefficients &coeffs, c128 pt);
+
+  /** evaluates polynomial with complex coefficients at complex point.
+      returns result
+  */
+  [[maybe_unused]] static c128 evaluateComplexPolynomial(const ComplexCoefficients &coeffs, c128 pt);
+
+  /** divides polynomial with real coefficients by linear term corresponding to
+      a complex point.
+      fills `quotient` with the complex coefficients of the quotient polynomial.
+      returns the remainder.
+  */
+  [[maybe_unused]] static c128 dividePolynomial(const Coefficients &coeffs, c128 pt, ComplexCoefficients &quotient);
+
+  /** divides polynomial with complex coefficients by linear term corresponding
+      to a complex point.
+      fills `quotient` with the complex coefficients of the quotient polynomial.
+      returns the remainder.
+  */
+  [[maybe_unused]] static c128 divideComplexPolynomial(const ComplexCoefficients &coeffs, c128 pt, ComplexCoefficients &quotient);
+
+  /** repeatedly divides polynomial with real coefficients by linear terms
+      corresponding to the value and multiplicity of `pt`.
+      fills `remainders` with the remainder of each division, in reverse order
+      so the remainders can be used as coefficients to the taylor approximation
+      of the polynomial about `pt.value`.
+  */
+  [[maybe_unused]] static void dividePolynomialByRoot(const Coefficients &coeffs, Root pt, ComplexCoefficients &remainders);
+
+  /** returns the weighted average of `oldRoot` and `addedRoot`, adding their
+      orders.
+      if one is complex and the other is real, will treat the complex root as a
+      pair of conjugates being combined with the real root (counts the complex
+      order as double).
+   */
+  [[maybe_unused]] static Root mergeRoot(Root oldRoot, Root addedRoot);
+
+  /** returns the center of the unique circle passing through 3 complex points */
+  [[maybe_unused]] static c128 centerOfCircleThroughPoints(c128 z0, c128 z1, c128 z2);
 
 #define SOLVER_DEFINE(Name)\
   }\
